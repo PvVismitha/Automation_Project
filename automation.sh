cea19344f5ae6bc42a164554c699752dafde7e0f
PvVismitha <93810706+PvVismitha@users.noreply.github.com>
2021-12-08 23:51:00 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -37,7 +37,21 @@ else
 	echo "Failed to create tar file"
 	exit 1
 fi
+if [ ! -f /etc/cron.d/automation ];then
+	echo "0 0 * * * sudo /root/Automation_Project/automation.sh $s3_bucket" > /etc/cron.d/automation
+fi
 aws s3 cp ${logfolder}.tar s3://${s3_bucket}/${logfolder}.tar
 if [ $? -eq 0 ];then
 	echo "Tar file uploaded to storage s3://${s3_bucket}"
 fi
+size=$(ls -lh $logfolder.tar | cut -d ' ' -f 5)
+if [ ! -f /var/www/html/inventory.html ];then
+	echo -e "<html>\n<body>\n<table>\n\t<tr>" > /var/www/html/inventory.html
+	echo -e "\t\t<th>Log Type</th>\n\t\t<th>Time Created</th>\n\t\t<th>Type</th>\n\t\t<th>Size</th>" >> /var/www/html/inventory.html
+	echo -e "\t</tr>" >> /var/www/html/inventory.html
+	echo -e "\t<tr>\n\t\t<td>http-logs</td>\n\t\t<td>${timestamp}</td>\n\t\t<td>tar</td>\n\t\t<td>${size}</td>\n\t</tr>\n" >> /var/www/html/inventory.html
+	echo -e "</table>\n</body>\n</html>" >> /var/www/html/inventory.html
+else
+	sed -i '/<\/table>/ i \t<tr>\n\t\t<td>http-logs</td>\n\t\t<td>${timestamp}</td>\n\t\t<td>tar</td>\n\t\t<td>${size}</td>\n\t</tr>\n' /var/www/html/inventory.html
+fi
+
